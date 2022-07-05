dbe2ee0e3f343db20e68b0518109712eb8ed39d9
Ali Rios <ali.rios@gmail.com>
2022-07-05 01:00:48 +0000

    challenges hackerRank

---
+++ list_comprehensions.py
@@ -0,0 +1,12 @@
+def build_coord(x, y, z, n):
+    coords = [[i, j, k] for i in range(x+1) for j in range(y) for k in range(z)]
+
+    def filter_coord(coord):
+        nonlocal n
+        return not sum(coords) == n
+
+    coords = list(filter(filter_coord, coords))
+    print(coords)
+
+if __name__ == '__main__':
+    build_coord(x, y, z, n)
